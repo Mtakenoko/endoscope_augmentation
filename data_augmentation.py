98ec342a99584751124110f9c9bc3359c56cd68c
Hayato Takeyama <takehaya.724@gmail.com>
2020-12-04 09:52:35 +0900

    色彩変更も追加+GRAYのバグ対処

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,11 +1,15 @@
 import os
+import shutil
 import cv2
+import numpy as np
 
 path = "eyemodel_train"
 path_raw = "eyemodel_train_raw"
 
-if not os.path.exists(path):
-    os.mkdir(path)
+if os.path.exists(path):
+    print('delete directory...')
+    shutil.rmtree(path)
+os.mkdir(path)
 
 files = os.listdir(path_raw)
 files_dir = [f for f in files if os.path.isdir(os.path.join(path_raw, f))]
@@ -39,26 +43,76 @@ for count, dir in enumerate(files_dir, 1):
             color_img_flip_udlr = cv2.flip(color_img, -1)
             depth_img_flip_udlr = cv2.flip(depth_img, -1)
 
+            # 画像の色彩変更
+            hsv_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2HSV)
+            img_h, img_s, img_v = cv2.split(hsv_img)
+            stride = 8
+            hsv_img_1 = cv2.merge(((img_h + stride*1) % 255, img_s, img_v))
+            hsv_img_2 = cv2.merge(((img_h + stride*2) % 255, img_s, img_v))
+            hsv_img_3 = cv2.merge(((img_h + stride*3) % 255, img_s, img_v))
+            hsv_img_4 = cv2.merge(((img_h + stride*4) % 255, img_s, img_v))
+            hsv_img_5 = cv2.merge(((img_h + stride*5) % 255, img_s, img_v))
+            hsv_img_6 = cv2.merge(((img_h + stride*6) % 255, img_s, img_v))
+            hsv_img_7 = cv2.merge(((img_h + stride*7) % 255, img_s, img_v))
+            hsv_img_8 = cv2.merge(((img_h + stride*8) % 255, img_s, img_v))
+            hsv_img_9 = cv2.merge(((img_h + stride*9) % 255, img_s, img_v))
+            hsv_img_10 = cv2.merge(((img_h + stride*10) % 255, img_s, img_v))
+            color_img_1 = cv2.cvtColor(hsv_img_1, cv2.COLOR_HSV2BGR)
+            color_img_2 = cv2.cvtColor(hsv_img_2, cv2.COLOR_HSV2BGR)
+            color_img_3 = cv2.cvtColor(hsv_img_3, cv2.COLOR_HSV2BGR)
+            color_img_4 = cv2.cvtColor(hsv_img_4, cv2.COLOR_HSV2BGR)
+            color_img_5 = cv2.cvtColor(hsv_img_5, cv2.COLOR_HSV2BGR)
+            color_img_6 = cv2.cvtColor(hsv_img_6, cv2.COLOR_HSV2BGR)
+            color_img_7 = cv2.cvtColor(hsv_img_7, cv2.COLOR_HSV2BGR)
+            color_img_8 = cv2.cvtColor(hsv_img_8, cv2.COLOR_HSV2BGR)
+            color_img_9 = cv2.cvtColor(hsv_img_9, cv2.COLOR_HSV2BGR)
+            color_img_10 = cv2.cvtColor(hsv_img_10, cv2.COLOR_HSV2BGR)
+
+            # GrayScaleに変換
+            depth_img = cv2.cvtColor(depth_img, cv2.COLOR_BGR2GRAY)
+            depth_img_rotate_90_clockwise = cv2.cvtColor(depth_img_rotate_90_clockwise, cv2.COLOR_BGR2GRAY)
+            depth_img_rotate_180_clockwise = cv2.cvtColor(depth_img_rotate_180_clockwise, cv2.COLOR_BGR2GRAY)
+            depth_img_rotate_270_clockwise = cv2.cvtColor(depth_img_rotate_270_clockwise, cv2.COLOR_BGR2GRAY)
+            depth_img_flip_ud = cv2.cvtColor(depth_img_flip_ud, cv2.COLOR_BGR2GRAY)
+            depth_img_flip_lr = cv2.cvtColor(depth_img_flip_lr, cv2.COLOR_BGR2GRAY)
+            depth_img_flip_udlr = cv2.cvtColor(depth_img_flip_udlr, cv2.COLOR_BGR2GRAY)
+
             # eyemodel_trainディレクトリに全て格納
-            if color_img.shape[0]==320 and color_img.shape[1]==320 and depth_img.shape[0]==320 and depth_img.shape[1]==320:
-                cv2.imwrite(path + '/' + dir + '/' + base +'.jpg', color_img)
-                cv2.imwrite(path + '/' + dir + '/' + base +'.png', depth_img)
-            if color_img_rotate_90_clockwise.shape[0]==320 and color_img_rotate_90_clockwise.shape[1]==320 and depth_img_rotate_90_clockwise.shape[0]==320 and depth_img_rotate_90_clockwise.shape[1]==320:
-                cv2.imwrite(path + '/' + dir + '/' + base + '_90' +'.jpg', color_img_rotate_90_clockwise)
-                cv2.imwrite(path + '/' + dir + '/' + base + '_90' +'.png', depth_img_rotate_90_clockwise)
-            if color_img_rotate_180_clockwise.shape[0]==320 and color_img_rotate_180_clockwise.shape[1]==320 and depth_img_rotate_180_clockwise.shape[0]==320 and depth_img_rotate_180_clockwise.shape[1]==320:
-                cv2.imwrite(path + '/' + dir + '/' + base + '_180' +'.jpg', color_img_rotate_180_clockwise)
-                cv2.imwrite(path + '/' + dir + '/' + base + '_180' +'.png', depth_img_rotate_180_clockwise)
-            if color_img_rotate_270_clockwise.shape[0]==320 and color_img_rotate_270_clockwise.shape[1]==320 and depth_img_rotate_270_clockwise.shape[0]==320 and depth_img_rotate_270_clockwise.shape[1]==320:
-                cv2.imwrite(path + '/' + dir + '/' + base + '_270' +'.jpg', color_img_rotate_270_clockwise)
-                cv2.imwrite(path + '/' + dir + '/' + base + '_270' +'.png', depth_img_rotate_270_clockwise)
-            if color_img_flip_ud.shape[0]==320 and color_img_flip_ud.shape[1]==320 and depth_img_flip_ud.shape[0]==320 and depth_img_flip_ud.shape[1]==320:
-                cv2.imwrite(path + '/' + dir + '/' + base + '_flip_ud' +'.jpg', color_img_flip_ud)
-                cv2.imwrite(path + '/' + dir + '/' + base + '_flip_ud' +'.png', depth_img_flip_ud)
-            if color_img_flip_lr.shape[0]==320 and color_img_flip_lr.shape[1]==320 and depth_img_flip_lr.shape[0]==320 and depth_img_flip_lr.shape[1]==320:
-                cv2.imwrite(path + '/' + dir + '/' + base + '_flip_lr' +'.jpg', color_img_flip_lr)
-                cv2.imwrite(path + '/' + dir + '/' + base + '_flip_lr' +'.png', depth_img_flip_lr)
-            if color_img_flip_udlr.shape[0]==320 and color_img_flip_udlr.shape[1]==320 and depth_img_flip_udlr.shape[0]==320 and depth_img_flip_udlr.shape[1]==320:
-                cv2.imwrite(path + '/' + dir + '/' + base + '_flip_udlr' +'.jpg', color_img_flip_udlr)
-                cv2.imwrite(path + '/' + dir + '/' + base + '_flip_udlr' +'.png', depth_img_flip_udlr)
+            cv2.imwrite(path + '/' + dir + '/' + base +'.jpg', color_img)
+            cv2.imwrite(path + '/' + dir + '/' + base +'.png', depth_img)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_90' +'.jpg', color_img_rotate_90_clockwise)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_90' +'.png', depth_img_rotate_90_clockwise)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_180' +'.jpg', color_img_rotate_180_clockwise)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_180' +'.png', depth_img_rotate_180_clockwise)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_270' +'.jpg', color_img_rotate_270_clockwise)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_270' +'.png', depth_img_rotate_270_clockwise)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_flip_ud' +'.jpg', color_img_flip_ud)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_flip_ud' +'.png', depth_img_flip_ud)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_flip_lr' +'.jpg', color_img_flip_lr)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_flip_lr' +'.png', depth_img_flip_lr)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_flip_udlr' +'.jpg', color_img_flip_udlr)
+            cv2.imwrite(path + '/' + dir + '/' + base + '_flip_udlr' +'.png', depth_img_flip_udlr)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(1) + '.jpg', color_img_1)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(1) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(2) + '.jpg', color_img_2)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(2) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(3) + '.jpg', color_img_3)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(3) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(4) + '.jpg', color_img_4)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(4) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(5) + '.jpg', color_img_5)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(5) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(6) + '.jpg', color_img_6)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(6) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(7) + '.jpg', color_img_7)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(7) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(8) + '.jpg', color_img_8)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(8) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(9) + '.jpg', color_img_9)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(9) + '.png', depth_img)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(10) + '.jpg', color_img_10)
+            # cv2.imwrite(path + '/' + dir + '/' + base + '_hue' + str(10) + '.png', depth_img)
+
+
+            
 print('Finished data augmentation!')
